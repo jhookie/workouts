02d1752dd7bde8301cb520b847dd07c7769c9ae9
jhookie <jeffhookie@gmail.com>
2026-05-25 22:04:41 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker: cache shell, network-first for API.
-const CACHE = 'workouts-v11';
+const CACHE = 'workouts-v11b';
 const SHELL = [
   './',
   './index.html',
