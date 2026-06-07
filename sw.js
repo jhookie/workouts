0bd5502ebd143e02ac88f2ba0dcec7812184d6f5
Jeff <jeffhookie@gmail.com>
2026-06-07 19:46:59 -0400

    Programs tab + lift session detail screen (v14+v15)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker: cache shell, network-first for API.
-const CACHE = 'workouts-v13';
+const CACHE = 'workouts-v15';
 const SHELL = [
   './',
   './index.html',
