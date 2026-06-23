886f358d5129f4ea657c88d0cec88f1732ed2825
Jeff <jeffhookie@gmail.com>
2026-06-23 10:23:41 -0400

    Lift logger: add editable date picker (v17.1)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker: cache shell, network-first for API.
-const CACHE = 'workouts-v17';
+const CACHE = 'workouts-v17-1';
 const SHELL = [
   './',
   './index.html',
