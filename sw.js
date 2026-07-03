185ce54074b2c4194adbf2df494c74be7f544618
Jeff <jeffhookie@gmail.com>
2026-07-03 16:17:45 -0400

    Segmented modality filter on Miles YTD (v20.3)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker: cache shell, network-first for API.
-const CACHE = 'workouts-v20-2';
+const CACHE = 'workouts-v20-3';
 const SHELL = [
   './',
   './index.html',
