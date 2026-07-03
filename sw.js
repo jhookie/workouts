df6c15a7147891958e5720b9ab6d794bcfe0d00e
Jeff <jeffhookie@gmail.com>
2026-07-03 10:50:06 -0400

    Onboarding wizard + modality schema (v19)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker: cache shell, network-first for API.
-const CACHE = 'workouts-v18';
+const CACHE = 'workouts-v19';
 const SHELL = [
   './',
   './index.html',
