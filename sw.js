753fa6d824b2ee259522902ef947dbb14524627f
Jeff <jeffhookie@gmail.com>
2026-07-03 15:26:02 -0400

    Modality-adaptive Home + goals + activity forms + feedback (v20)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker: cache shell, network-first for API.
-const CACHE = 'workouts-v19-3';
+const CACHE = 'workouts-v20';
 const SHELL = [
   './',
   './index.html',
