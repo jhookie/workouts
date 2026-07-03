692b06aa3a6c0036b4aeea8641757544584dd786
Jeff <jeffhookie@gmail.com>
2026-07-03 10:54:37 -0400

    Fix el() null-attribute bug (v19.1)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker: cache shell, network-first for API.
-const CACHE = 'workouts-v19';
+const CACHE = 'workouts-v19-1';
 const SHELL = [
   './',
   './index.html',
