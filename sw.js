e965e4eaf38937ea5b961348c6cab44fcd087b60
Jeff <jeffhookie@gmail.com>
2026-07-05 13:00:35 -0400

    Configurable Home widgets (v23)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker: cache shell, network-first for API.
-const CACHE = 'workouts-v22';
+const CACHE = 'workouts-v23';
 const SHELL = [
   './',
   './index.html',
