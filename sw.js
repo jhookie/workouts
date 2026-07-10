6f3068c3817268dfce6f44eb1d8544ac2c23a7d2
Jeff <jeffhookie@gmail.com>
2026-07-10 16:22:58 -0400

    v32.4.1: fetch plan_type in reads (fixes reclassify revert-on-navigate)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first service worker: cache shell, network-first for API.
-const CACHE = 'loggbok-v32-4';
+const CACHE = 'loggbok-v32-4-1';
 const SHELL = [
   './',
   './index.html',
